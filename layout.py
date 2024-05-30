e1814ce844a68381d81ccdc14f1bf8bd232a87e1
Taco <87836366+Tacotakedown@users.noreply.github.com>
2024-05-30 01:08:08 -0600

    convert to webp automatically

--- layout.py
+++ layout.py
@@ -1,28 +1,47 @@
 import os
 import json
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+try:
+    from PIL import Image
+except ImportError:
+    install("Pillow")
+    from PIL import Image
 
 repo_owner = "infinity-MSFS"
 repo_name = "groups"
 branch = "main"
-
 
 links = {}
 
 def generate_jsdelivr_url(file_path):
     return f"https://cdn.jsdelivr.net/gh/{repo_owner}/{repo_name}@{branch}/{file_path}"
 
-    
 for root, dirs, files in os.walk("."):
     for file in files:
         if file.endswith(".png"):
-            relative_path = os.path.relpath(os.path.join(root, file), ".")
-            relative_path = relative_path.replace("\\", "/")
-            url = generate_jsdelivr_url(relative_path)
-            links[relative_path] = url
+            png_path = os.path.join(root, file)
+            webp_path = os.path.splitext(png_path)[0] + ".webp"
             
-            
+            try:
+                png_image = Image.open(png_path)
+                png_image.save(webp_path, 'WEBP')
+                print(f"Converted {png_path} to {webp_path}")
+            except Exception as e:
+                print(f"Failed to convert {png_path}: {e}")
+                continue
+
+            relative_webp_path = os.path.relpath(webp_path, ".")
+            relative_webp_path = relative_webp_path.replace("\\", "/")
+            url = generate_jsdelivr_url(relative_webp_path)
+            links[relative_webp_path] = url
+
 output_file = "jsdeliver_links.json"
 with open(output_file, "w") as f:
-    json.dump(links, f, indent = 4)
-    
+    json.dump(links, f, indent=4)
+
 print(f"JSON file generated: {output_file}")
